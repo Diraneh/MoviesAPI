a16cc9bca9ee34b441ad213ee643658a25a02616
diraneh <diraneh>
2020-12-08 12:10:46 +0100

    adding jsobject

--- movies.js
+++ movies.js
@@ -1,22 +1,25 @@
-var titlesElt = document.getElementById("title");
+
 ajaxGet("https://api.themoviedb.org/3/movie/popular?api_key=2bb0925fc8fc15c40b13b090730ac54e&language=us&page=1", function (reponse) {
     // Transforme la réponse en un tableau d'titles
     var title= JSON.parse(reponse);
-    
-    
+    var data=title['results'];
     console.log(title);
+    var titlesElt = document.getElementById("film");
         // Ajout du titre et du contenu de chaque article
-        var contenuElt = document.createElement("div");
+        for(var i=0; i<20;i++){
+            var contenuElt = document.createElement("div");
        
-        contenuElt.textContent = title.contenu;
-        var titreElt = document.createElement("h2");
-    
-
-        titreElt.textContent = title.titre;
-       
+            var titreElt = document.createElement("h2");
         
-        contenuElt.append(titreElt);
-        console.log(contenuElt);
+    
+            titreElt.textContent = data[i].title;
+           
+            titlesElt.appendChild(contenuElt);
+            contenuElt.appendChild(titreElt);
+            console.log(titreElt);
+
+        }
+       
        
     });
 

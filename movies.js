78aa1b3001b21303f18c140d6c4df8d37d688ca2
diraneh <diraneh>
2020-12-09 12:30:55 +0100

    adding modal and logo

--- movies.js
+++ movies.js
@@ -17,43 +17,44 @@ ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=2bb0925fc8fc15c40b1
             img.src = "https://image.tmdb.org/t/p/w500"+data[i].poster_path+"";       
             
            
-            // var contenuElts=document.createElement("p");
-             var titreElt=document.createElement("h6");
-             titreElt.textContent=data[i].title;
-            titlesElt.appendChild(contenuElt);
-            contenuElt.appendChild(img);
-            //contenuEltsim.appendChild(img);
-          //  img.appendChild(contenuElts);
             
-          img.appendChild(titreElt);
          
            
-            console.log(titreElt);
-            // Get the modal
-var modal = document.getElementById("myModal");
+         
+            var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("film");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+           
+            
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+            
+            // When the user clicks the button, open the modal 
+            titlesElt.onclick = function() {
+              modal.style.display = "block";
+              
+             
+            } 
+            
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+              modal.style.display = "none";
+            }
+            
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            }
+            var modalo = document.getElementById("detaille");
+            var titreElt=document.createElement("p");
+            titreElt.textContent=data[i].title;
+            
+           titlesElt.appendChild(contenuElt);
+           contenuElt.appendChild(img);
+           
+         img.appendChild(modalo);
+        
           
         }
      

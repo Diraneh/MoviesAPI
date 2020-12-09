ea2b8e91a5c590193b2438163a24a27496244e10
diraneh <diraneh>
2020-12-09 18:24:06 +0100

    adding modal and logo

--- movies.js
+++ movies.js
@@ -1,5 +1,5 @@
 
-ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=2bb0925fc8fc15c40b13b090730ac54e&language=en-US&page=1", function (reponse) {
+ajaxGet("https://api.themoviedb.org/3/movie/popular?api_key=2bb0925fc8fc15c40b13b090730ac54e&language=us&page=1", function (reponse) {
     // Transforme la réponse en un tableau d'titles
     
     var titlesElt = document.getElementById("film");
@@ -9,7 +9,7 @@ ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=2bb0925fc8fc15c40b1
             var title= JSON.parse(reponse);
     var data=title['results'];
     console.log(title);
-           var contenuElt = document.createElement("p");
+           var contenuElt = document.createElement("div");
            contenuElt.classList="col-3 col-md-3 col-lg-3";
            
            var img=document.createElement("img");
@@ -17,54 +17,53 @@ ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=2bb0925fc8fc15c40b1
         
             img.src = "https://image.tmdb.org/t/p/w500"+data[i].poster_path+"";       
             
-           
             var titreElt=document.createElement("p");
             titreElt.textContent=data[i].title;
             
             titreElt.style="color:white;padding-bottom:20px;margin-top:0;margin-bottom:-1rem"
-          
-
-
-             // Simulate a code delay
-             setTimeout( function(){
-                var modal = document.getElementById("myModal");
-                var boddy = document.getElementById("body");
-              //  var te=document.createElement("p");
-                boddy.textContent =data[i].title;
-               
-              
-                
-                // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
-                
-                // When the user clicks the button, open the modal 
-                img.onclick = function() {
-                  modal.style.display = "block";
-                  
-                 
-                } 
-                
-                
-                
-                // When the user clicks anywhere outside of the modal, close it
-                window.onclick = function(event) {
-                  if (event.target == modal) {
-                    modal.style.display = "block";
-                  }
-                }
-                callback(modal);
-             }, 1500 );
             
-         
+            
+
+            
+            var modal = document.getElementById("myModal");
+            var boddy = document.getElementById("body");
+            var te=document.createElement("p");
+            te.textContent =data[i].title;
+            
+          
+            // When the user clicks on <span> (x), close the modal
+img.onclick = function() {
+  modal.style.display = "none";
+}
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+            
+            // When the user clicks the button, open the modal 
+            img.onclick = function() {
+              modal.style.display = "block";
+              
+             
+            } 
+            
+            
+            
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "block";
+              }
+            }
+        
            titlesElt.append(contenuElt);
            contenuElt.appendChild(img);
            contenuElt.appendChild(titreElt);
            contenuElt.appendChild(modal);
-      
+          modal.appendChild(boddy);
+          boddy.appendChild(te);
        
-        
-          
         }
+          
+        
      
     });
 

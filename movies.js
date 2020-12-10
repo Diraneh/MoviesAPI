d8a413467f350ad6f29a8f72c49062d0404b25fc
diraneh <diraneh>
2020-12-10 05:38:47 +0100

    modal

--- movies.js
+++ movies.js
@@ -1,14 +1,38 @@
 
+
+var UniqueRandom = {
+            NumHistory: new Array(),
+            generate: function (maxNum) {
+                var current = Math.ceil(Math.random() * (maxNum));
+                if (maxNum > 1 && this.NumHistory.length > 0) {
+                    if (this.NumHistory.length != maxNum) {
+                        while ($.inArray(current, this.NumHistory) != -1) {
+                            current = Math.ceil(Math.random() * (maxNum));
+                        }
+                        this.NumHistory.push(current);
+                        return current;
+                    } else {
+                      
+                        return current;
+                    }
+                } else {
+                   
+                    this.NumHistory.push(current);
+                    return current;
+                }
+            }
+            
+        };
 ajaxGet("https://api.themoviedb.org/3/movie/popular?api_key=2bb0925fc8fc15c40b13b090730ac54e&language=us&page=1", function (reponse) {
     // Transforme la réponse en un tableau d'titles
-    
+    var title= JSON.parse(reponse);
+    var data=title['results'];
+    console.log(title);
     var titlesElt = document.getElementById("film");
     
         // Ajout du titre et du contenu de chaque article
-        for(var i=0; i<20;i++){
-            var title= JSON.parse(reponse);
-    var data=title['results'];
-    console.log(title);
+        for (var i = 0; i <data.length; i++){
+          
            var contenuElt = document.createElement("div");
            contenuElt.classList="col-3 col-md-3 col-lg-3";
            
@@ -22,14 +46,18 @@ ajaxGet("https://api.themoviedb.org/3/movie/popular?api_key=2bb0925fc8fc15c40b13
             
             titreElt.style="color:white;padding-bottom:20px;margin-top:0;margin-bottom:-1rem"
             
-           
-
+            titlesElt.append(contenuElt);
+            contenuElt.appendChild(img);
+            contenuElt.appendChild(titreElt);
             
+             
+         if(data[i]!==data[0]){
+
+         
             var modal = document.getElementById("myModal");
             var boddy = document.getElementById("body");
-            var te=document.createElement("p");
-            te.textContent =data[i].title;
-           
+          
+        
           
             // When the user clicks on <span> (x), close the modal
 img.onclick = function() {
@@ -41,7 +69,13 @@ img.onclick = function() {
             // When the user clicks the button, open the modal 
             img.onclick = function() {
               modal.style.display = "block";
-              
+              var te=document.createElement("div");
+            te.textContent =data[1].title;
+            
+           contenuElt.appendChild(modal);
+           modal.appendChild(boddy);
+           boddy.appendChild(te);
+        
              
             } 
             
@@ -53,15 +87,84 @@ span.onclick = function() {
 }
 
 
+};      
+
+
+if(data[i]!==data[1]){
+var modal = document.getElementById("myModal");
+var boddy = document.getElementById("body");
+
+
+
+// When the user clicks on <span> (x), close the modal
+img.onclick = function() {
+modal.style.display = "none";
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+img.onclick = function() {
+  modal.style.display = "block";
+  var tee=document.createElement("div");
+tee.textContent =data[0].title;
+
+contenuElt.appendChild(modal);
+modal.appendChild(boddy);
+boddy.appendChild(tee);
+
+ 
+} 
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+
+}
         
-           titlesElt.append(contenuElt);
-           contenuElt.appendChild(img);
-           contenuElt.appendChild(titreElt);
-           contenuElt.appendChild(modal);
-          modal.appendChild(boddy);
-          boddy.appendChild(te);
-       
-       
+   
+
+
+
+if(data[i]!==data[1] && data[i]!==data[0] ){
+  var modal = document.getElementById("myModal");
+  var boddy = document.getElementById("body");
+  
+  
+  
+  // When the user clicks on <span> (x), close the modal
+  img.onclick = function() {
+  modal.style.display = "none";
+  }
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  img.onclick = function() {
+    modal.style.display = "block";
+    var tee=document.createElement("div");
+  tee.textContent =data[2].title;
+  
+  contenuElt.appendChild(modal);
+  modal.appendChild(boddy);
+  boddy.appendChild(tee);
+  
+   
+  } 
+  
+  
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+  modal.style.display = "none";
+  }
+  
+  
+  }
           
         }
      
@@ -70,6 +173,7 @@ span.onclick = function() {
 
 
     
+  
 
   
             
